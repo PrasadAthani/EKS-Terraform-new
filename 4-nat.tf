d7f0ee4cd8663cb6d70324163de93f6ac90914bb
PrasadAthani <150219751+PrasadAthani@users.noreply.github.com>
2024-10-07 15:24:09 +0530

    Update 4-nat.tf

--- 4-nat.tf
+++ 4-nat.tf
@@ -2,7 +2,7 @@ resource "aws_eip" "nat" {
   vpc = true
 
   tags = {
-    Name = "nat"
+    Name = "var.cluser_name-nat"
   }
 }
 
@@ -11,7 +11,7 @@ resource "aws_nat_gateway" "nat" {
   subnet_id     = aws_subnet.public-subnet-a.id
 
   tags = {
-    Name = "nat"
+    Name = "var.cluster_name-nat"
   }
 
   depends_on = [aws_internet_gateway.igw]
