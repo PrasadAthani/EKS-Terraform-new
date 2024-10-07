3ce6707bda87c7a3e521a478d5372c555dbbfb2b
PrasadAthani <150219751+PrasadAthani@users.noreply.github.com>
2024-10-07 15:12:07 +0530

    Update 11-iam-lb-controller.tf

--- 11-iam-lb-controller.tf
+++ 11-iam-lb-controller.tf
@@ -18,12 +18,12 @@ data "aws_iam_policy_document" "aws_load_balancer_controller_assume_role_policy"
 
 resource "aws_iam_role" "aws_load_balancer_controller" {
   assume_role_policy = data.aws_iam_policy_document.aws_load_balancer_controller_assume_role_policy.json
-  name               = "aws-load-balancer-controller"
+  name               = "cluster_name"
 }
 
 resource "aws_iam_policy" "aws_load_balancer_controller" {
   policy = file("./AWSLoadBalancerController.json")
-  name   = "AWSLoadBalancerController"
+  name   = "cluster_name"
 }
 
 resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller_attach" {
